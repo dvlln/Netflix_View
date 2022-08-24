fb1cc262531847136fbb1a1e52d63d948e46f949
Allan Franco <allan.work19@gmail.com>
2022-08-23 22:00:38 -0300

    Listagem de filmes da netflix no console

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Tmdb from './Tmdb';
 
-function App() {
+export default() => {
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState([]);
+  
+  useEffect(() => {
+    const loadAll = async() => {
+    let list = await Tmdb.getHomeList();
+
+    console.log(list);
+
+    setMovieList(list);
+    let originals = list.filter(i => i.slug === 'originals');
+    let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+    let chosen = originals[0].items.results[randomChosen];
+    let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+
+    console.log(chosenInfo);
+
+    setFeaturedData(chosenInfo);
+
+    }
+    loadAll();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
-
-export default App;
---
+++ src/Tmdb.js
@@ -0,0 +1,75 @@
+const api_key = '34465c5b9c6e892b6cd7310bedc93150';
+const api_base = 'https://api.themoviedb.org/3';
+
+// async = get request and wait
+const basicFetch = async(endpoint) => {
+    const req = await fetch(`${api_base}${endpoint}`); // criando URL de busca
+    const json = await req.json(); // transforma a URL em json
+
+    return json;
+}
+
+export default {
+    getHomeList: async() => {
+        return [
+            {
+                slug: 'originals',
+                title: 'Originais do Netflix',
+                items: await basicFetch(`/discover/tv?with_network=213&language=pt-BR&api_key=${api_key}`)
+            },
+            {
+                slug: 'trending',
+                title: 'Recomendados para você',
+                items: await basicFetch(`/trending/all/week?language=pt-BR&api_key=${api_key}`)
+            },
+            {
+                slug: 'toprated',
+                title: 'Em alta',
+                items: await basicFetch(`/movie/top_rated?&language=pt-BR&api_key=${api_key}`)
+            },
+            {
+                slug: 'action',
+                title: 'Ação',
+                items: await basicFetch(`/discover/movie?with_genres=28&language=pt-BR&api_key=${api_key}`)
+            },
+            {
+                slug: 'comedy',
+                title: 'Comédia',
+                items: await basicFetch(`/discover/movie?with_genres=35&language=pt-BR&api_key=${api_key}`)
+            },
+            {
+                slug: 'horror',
+                title: 'Terror',
+                items: await basicFetch(`/discover/movie?with_genres=27&language=pt-BR&api_key=${api_key}`)
+            },
+            {
+                slug: 'romance',
+                title: 'Romance',
+                items: await basicFetch(`/discover/movie?with_genres=10749&language=pt-BR&api_key=${api_key}`)
+            },
+            {
+                slug: 'documentary',
+                title: 'Documentários',
+                items: await basicFetch(`/discover/movie?with_genres=99&language=pt-BR&api_key=${api_key}`)
+            },
+        ]
+    },
+
+    getMovieInfo: async(movieId, type) => {
+        let info = {};
+        if(movieId) {
+            switch(type){
+                case 'movie':
+                    info = await basicFetch(`/movie/${movieId}?language=pt-BR&api_key=${api_key}`)
+                    break
+                case 'tv':
+                    info = await basicFetch(`/tv/${movieId}?language=pt-BR&api_key=${api_key}`)
+                    break
+                default:
+                    info = null
+                    break
+            }
+        }
+        return info;
+    }
+}
